84f97e1776f4c1a23b4a1f0e67775c6298e3c352
Amaan Supariwala <amaan.supariwala@empowerate.com>
2020-08-23 22:37:04 -0400

    First Try

---
+++ precache-manifest.b150beac029520cc8b772c7b2ee05ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c28100d4a2157b9eb4100d26e731ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5315ff450721af9d70fe",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5315ff450721af9d70fe",
+    "url": "/static/js/main.17520811.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
