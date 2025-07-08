a9fbdf3283936204643904131d4d03057aa6d805
amaansupariwala <amaan.supariwala@gmail.com>
2025-07-07 22:17:48 -0700

    Fix: Remove unused motion import from Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link, useLocation } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import { Menu, X } from 'lucide-react';
 
 const HeaderContainer = styled.header`
